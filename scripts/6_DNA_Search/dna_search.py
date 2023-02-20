ad1ed3d8bc7d44b6a9507a27749843c3f462eb18
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2023-02-20 05:15:25 +0000

    Continuing - added string to gene function definition.

--- scripts/6_DNA_Search/dna_search.py
+++ scripts/6_DNA_Search/dna_search.py
@@ -8,3 +8,16 @@ comparative operators such as = < > etc for free.'''
 
 ''' Context for DNA - a Codon is a Tuple of three Nucleotides '''
 
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide] # Type alias for Codon
+Gene = List(Codon) # Type alias for genes
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+def string_to_gene(s: str):
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s): # Don't run off the end
+            return gene
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i+1]], Nucleotide[s[i+2]])
+        gene.append(codon)
+        return gene
