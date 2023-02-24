7c5e11707e62f6a9758162c8a1cb3b523765eaab
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2023-02-24 06:13:44 +0000

    Final version of DNA search

--- scripts/6_DNA_Search/dna_search.py
+++ scripts/6_DNA_Search/dna_search.py
@@ -9,7 +9,7 @@ comparative operators such as = < > etc for free.'''
 ''' Context for DNA - a Codon is a Tuple of three Nucleotides '''
 
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide] # Type alias for Codon
-Gene = List(Codon) # Type alias for genes
+Gene = List[Codon] # Type alias for genes
 
 gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
 
@@ -21,3 +21,34 @@ def string_to_gene(s: str):
         codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i+1]], Nucleotide[s[i+2]])
         gene.append(codon)
         return gene
+
+my_gene: Gene = string_to_gene(gene_str)
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+
+print(linear_contains(my_gene, acg)) # True
+print(linear_contains(my_gene, gat)) # False
+
+def binary_contains(gene: Gene, key_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high: # while there is still a search space
+        mid: int = (low + high)  // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+my_sorted_gene: Gene = sorted(my_gene)
+print(binary_contains(my_sorted_gene, acg))  # True
+print(binary_contains(my_sorted_gene, gat))  # False
